76de4d4d2030df113dac15a0c0f519e173eb8524
CrankyHippo <crankyhippo@gmail.com>
2022-08-26 13:46:45 -0700

    update

---
+++ resource-main.js
@@ -0,0 +1,650 @@
+// Default function get element and change style to "hidden"
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Hide meet UI (Updated: 6/14/2022, made into 6 bookmarks in “Scripts (NEW)” folder)
+
+// Position 1080p (6/14/22):
+javascript: (function () {
+  var d = document;
+  e = d.getElementsByClassName("axUSnc")[0];
+  e.style = "width: 1280px; height: 720px; left: 16px; top: 16px;";
+  z = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  z.style = "width: 1280px; height: 720px; left: 0px; top: 0px;";
+  y = d.getElementsByClassName("koV58 Zi94Db S7urwe")[0];
+  y.style = "width: 1280px; height: 720px; left: 0px; top: 0px;";
+})();
+
+// Position 4K (6/14/22):
+javascript: (function () {
+  var d = document;
+  e = d.getElementsByClassName("axUSnc")[0];
+  e.style = "width: 1440px; height: 810px; left: 16px; top: 16px;";
+  z = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  z.style = "width: 1440px; height: 810px; left: 16px; top: 16px;";
+  y = d.getElementsByClassName("koV58 Zi94Db S7urwe")[0];
+  y.style = "width: 1440px; height: 810px; left: 16px; top: 16px;";
+})();
+
+// Remove Name (6/14/22):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre")[0];
+  e.style = "display: none;";
+})();
+
+// Remove Speaker Icon (6/14/22):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName(".JHK7jb")[0];
+  e2 = d.querySelector(".FTMc0c");
+  e3 = d.querySelector(".OGbUle");
+  e2.style = "display: none;";
+  e3.style = "display: none;";
+})();
+
+// Self-View (6/14/22):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("aGWPv")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Companion Mode (6/14/22):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("J0M6X ZmuLbd Didmac G03iKb")[0];
+  e.style = "display: none;";
+})();
+
+// Show Meet UI:
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "visible";
+})();
+
+//Barco settings: Zoom - 2880x1620, Pan - 454x244
+
+// Hide meet UI (NEW NEW Updated: 06/14/2021)
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("zWfAib Z319Jd n9oEIb QhPhw a1pVef")[0];
+  e.style = "width: 1280px; height: 720px; left: 16px; top: 16px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  e.style = "width: 1280px; height: 720px; left: 16px; top: 16px;";
+})();
+j;
+
+avascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("aGWPv")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("atLQQ")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("atLQQ kssMZb")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("JHK7jb Nep7Ue")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Hide Meet UI: (New Meet, offset ROI by 16x, 16y)
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("aGWPv")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("atLQQ")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("atLQQ kssMZb")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("JHK7jb Nep7Ue")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("zWfAib Z319Jd n9oEIb QhPhw a1pVef")[0];
+  e.style = "width: 1280px; height: 720px; left: 16px; top: 16px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  e.style = "width: 1280px; height: 720px; left: 16px; top: 16px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Elements 1: (OLD)
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Elements 2: (OLD)
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("aGWPv")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getEElementsementsByClassName("atLQQ")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("atLQQ kssMZb")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("JHK7jb Nep7Ue")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Hide Meet UI: (Old Meet)
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("zWfAib Z319Jd n9oEIb QhPhw a1pVef")[0];
+  e.style = "width: 1280px; height: 720px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  e.style = "width: 1280px; height: 720px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Hide Meet UI (Valley Oak) (1440x810):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("zWfAib Z319Jd n9oEIb QhPhw a1pVef")[0];
+  e.style = "width: 1440px; height: 810px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  e.style = "width: 1440px; height: 810px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Hide Meet UI (Grid View):
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
+
+// Show Meet UI:
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "visible";
+})();
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "visible";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "visible";
+})();
+
+// Extras:
+// Hand raised/user left/user muted (Needs to be run once when a hand is raised)
+
+javascript: (function () {
+  setInterval(
+    (function interval() {
+      var d = document,
+        e = d.getElementsByClassName("Mp2Z0b")[0];
+      e.style.visibility = "hidden";
+      return interval;
+    })(),
+    1
+  );
+})();
+
+// Wants to join (Needs to be run once when someone asks to join)
+
+javascript: (function () {
+  setInterval(
+    (function interval() {
+      var d = document,
+        e = d.getElementsByClassName("llhEMd iWO5td")[0];
+      e.style.visibility = "hidden";
+      return interval;
+    })(),
+    1
+  );
+})();
+
+// OLD SCRIPT NO CROP
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("pHsCke")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rG0ybd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("NSvDmb cM3h5d")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("ZY8hPc gtgjre pZFrDd")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("P48BS")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("rKOYsc")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("zWfAib Z319Jd n9oEIb QhPhw a1pVef")[0];
+  e.style = "width: 1920px; height: 1080px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("p2hjYe TPpRNe")[0];
+  e.style = "width: 1920px; height: 1080px; left: 0px; top: 0px;";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("Ijo7j")[0];
+  e.style.visibility = "hidden";
+})();
+
+javascript: (function () {
+  var d = document,
+    e = d.getElementsByClassName("l371ae")[0];
+  e.style.visibility = "hidden";
+})();
